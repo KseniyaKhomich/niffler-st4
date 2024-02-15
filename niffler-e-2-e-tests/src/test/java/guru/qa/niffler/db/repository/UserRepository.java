029becad24127af08f6c63e0984153e714cbd16f
Ksenia Homich <ksenia.homich@vrpconsulting.com>
2024-02-15 22:57:37 +0100

    added changes

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/repository/UserRepository.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/repository/UserRepository.java
@@ -1,8 +1,5 @@
 package guru.qa.niffler.db.repository;
 
-import guru.qa.niffler.config.Config;
-import guru.qa.niffler.config.DockerConfig;
-import guru.qa.niffler.config.LocalConfig;
 import guru.qa.niffler.db.model.UserAuthEntity;
 import guru.qa.niffler.db.model.UserEntity;
 
@@ -12,13 +9,20 @@ import java.util.UUID;
 public interface UserRepository {
 
   static UserRepository getInstance() {
-    if (System.getProperty("repository") == null) {
-      throw new RuntimeException("Repository parameter isn't found");
-    }
+    String repositoryParameter = System.getProperty("repository");
 
-    return System.getProperty("repository").equals("jdbc")
-            ? new UserRepositoryJdbc()
-            : new UserRepositorySJdbc();
+    if (repositoryParameter != null) {
+      return switch (repositoryParameter) {
+        case "jdbc" -> new UserRepositoryJdbc();
+        case "sjdbc" -> new UserRepositorySJdbc();
+        case "hibernate" -> new UserRepositoryHibernate();
+        default -> throw new IllegalArgumentException("Incorrect value for Repository Parameter.");
+      };
+
+    } else {
+      throw new IllegalArgumentException("Repository parameter isn't found.");
+
+    }
   }
 
   UserAuthEntity createInAuth(UserAuthEntity user);
