ff580ff063c00a0404a5ee6efae14e3ac685d7fa
dtuchs <dtuchs@gmail.com>
2024-02-20 10:04:47 +0100

    Oauth (vol 9)
    (cherry picked from commit 78f26d80a9bdff31783908ec6b3641fb045cf0ad)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/DbUser.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/DbUser.java
@@ -3,16 +3,17 @@ package guru.qa.niffler.jupiter.annotation;
 import guru.qa.niffler.jupiter.extension.DBUserExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import guru.qa.niffler.jupiter.extension.CreateUserExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(
-    RetentionPolicy.RUNTIME
-)
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@ExtendWith(DBUserExtension.class)
+@ExtendWith({DBUserExtension.class, CreateUserExtension.class})
 public @interface DbUser {
   String username() default "";
 
