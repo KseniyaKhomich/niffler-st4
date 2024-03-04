8b878803dbe399668c5b99d77ed887eb0d84caea
Ksenia Homich <ksenia.homich@vrpconsulting.com>
2024-03-04 12:41:30 +0100

    updated pages

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/DatabaseSpendExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/DatabaseSpendExtension.java
@@ -3,16 +3,17 @@ package guru.qa.niffler.jupiter;
 import guru.qa.niffler.db.model.CategoryEntity;
 import guru.qa.niffler.db.model.SpendingEntity;
 import guru.qa.niffler.db.repository.SpendingRepository;
+import guru.qa.niffler.jupiter.extension.SpendExtension;
 import guru.qa.niffler.model.SpendJson;
 
 import java.io.IOException;
 
-public class DatabaseSpendExtension extends SpendExtension{
+public class DatabaseSpendExtension extends SpendExtension {
 
 	private final SpendingRepository repository = SpendingRepository.getInstance();
 
 	@Override
-	SpendJson create(SpendJson spend) throws IOException {
+	public SpendJson create(SpendJson spend) throws IOException {
 		CategoryEntity categoryEntity = new CategoryEntity();
 		categoryEntity.setUsername(spend.username());
 		categoryEntity.setCategory(spend.category());
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendExtension.java
@@ -1,17 +1,13 @@
 package guru.qa.niffler.jupiter.extension;
 
 import guru.qa.niffler.api.spend.SpendClient;
-import guru.qa.niffler.jupiter.annotation.GenerateCategory;
 import guru.qa.niffler.jupiter.annotation.GenerateSpend;
-import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
-import guru.qa.niffler.model.UserJson;
-import okhttp3.OkHttpClient;
+
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.platform.commons.support.AnnotationSupport;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+
 
 import java.io.IOException;
 import java.util.Date;
@@ -23,6 +19,8 @@ public abstract class SpendExtension implements BeforeEachCallback {
       = ExtensionContext.Namespace.create(SpendExtension.class);
 
   private final SpendClient spendClient = new SpendClient();
+
+  public abstract SpendJson create(SpendJson spend) throws IOException;
 
   @Override
   public void beforeEach(ExtensionContext extensionContext) throws Exception {
@@ -46,7 +44,7 @@ public abstract class SpendExtension implements BeforeEachCallback {
 
       SpendJson created = spendClient.addSpend(spendJson);
       extensionContext.getStore(NAMESPACE)
-          .put(extensionContext.getUniqueId(), createdSpend);
+          .put(extensionContext.getUniqueId(), created);
     }
   }
 }
